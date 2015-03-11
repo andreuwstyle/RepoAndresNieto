57a9e7916d33bce73dcd201de54e9c5025c76c94
Administrador <Administrador@shadow_lite_sp3>
2015-03-11 09:32:25 +0100

    Si lo tocas te curto

---
+++ RepoAndresNieto/src/MiClase/masterclass.java
@@ -0,0 +1,10 @@
+package MiClase;
+
+public class masterclass {
+
+	public static void main(String[] args) {
+		System.out.println("Este es el primer proyecto de Andres Nieto");
+		System.out.println("Si lo tocas te curto");
+	}
+
+}
