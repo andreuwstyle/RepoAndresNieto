ca4e3b964bbf6a29646db2054ff82423cb0c7a73
Andres <Andres@eclipse.com>
2015-03-11 09:50:58 +0100

    Segundo commit

--- RepoAndresNieto/src/MiClase/masterclass.java
+++ RepoAndresNieto/src/MiClase/masterclass.java
@@ -6,6 +6,7 @@ public class masterclass {
 		System.out.println("Este es el primer proyecto de Andres Nieto");
 		System.out.println("Si lo tocas te curto");
 		System.out.println("No es broma");
+		
 	}
 
 }
---
+++ RepoAndresNieto/src/MiClase/secodclass.java
@@ -0,0 +1,7 @@
+package MiClase;
+
+public class secodclass {
+	public static void main(String[] args) {
+		System.out.println("Ahora toca base de datos");
+	}
+}
