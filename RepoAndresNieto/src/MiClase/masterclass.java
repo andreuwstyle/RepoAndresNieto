70100d288357df7e29d8d34e805087970ad94e98
Andres <Andres@eclipse.com>
2015-03-11 09:34:09 +0100

    No es broma

--- RepoAndresNieto/src/MiClase/masterclass.java
+++ RepoAndresNieto/src/MiClase/masterclass.java
@@ -5,6 +5,7 @@ public class masterclass {
 	public static void main(String[] args) {
 		System.out.println("Este es el primer proyecto de Andres Nieto");
 		System.out.println("Si lo tocas te curto");
+		System.out.println("No es broma");
 	}
 
 }
